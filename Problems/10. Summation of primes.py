befa18b9e8dedf1fb1dad6e52f59c50985cb5906
Paul J <jefferie@umich.edu>
2016-01-13 01:04:43 -0500

    Updated solution to problem 10 to use sieve of E, speeding-up by approx. 100x

--- Problems/10. Summation of primes.py
+++ Problems/10. Summation of primes.py
@@ -14,6 +14,42 @@ Find the sum of all the primes below two million.
 
 from time import time
 
+#Fast method using the sieve of Eratosthenes
+
+if __name__ == '__main__':
+    maxValueOfPrime = 2*10**6 - 1
+    incrementToPrint = 1000
+    startTime = time()
+    sumOfPrimes = 0
+    counter = 0
+    primeValues = [True] * (maxValueOfPrime + 1)
+    for aPrime in range(2,int(maxValueOfPrime**(1/2))+1):
+        if primeValues[aPrime]:     #don't check if already rulled-out
+            print("Found a prime:", aPrime)
+            if counter == 1000:
+                couner = 0
+                print("aPrime:", aPrime)
+            for numToElim in range(aPrime*2,maxValueOfPrime+1,aPrime):
+                primeValues[numToElim] = False
+        counter += 1
+    for aPrime in range(2, maxValueOfPrime+1):
+        if primeValues[aPrime]:
+            sumOfPrimes += aPrime
+    totalTime = time() - startTime
+    print("The sum of all primes less than", maxValueOfPrime, "is:", sumOfPrimes)
+    print("Time to find:", totalTime)
+    
+"""
+Result - quick method:
+
+The sum of all primes less than 1999999 is: 142913828922
+Time to find: 2.053637981414795
+"""
+
+
+"""
+
+The slow way
 def isANewPrime(number, lowerPrimes):
     sqrtNumber = number**(1/2)
     #print("number:", number, ", sqrtNumber:", sqrtNumber)
@@ -56,7 +92,9 @@ if __name__ == '__main__':
     totalTime = time() - startTime
     print("The sum of all primes less than", maxValueOfPrime, "is:", sumOfPrimesToLimit)
     print("Time to find:", totalTime)
-    
+
+
+"""
 """
 Result:
 
@@ -64,4 +102,5 @@ The sum of all primes less than 1999999 is: 142913828922
 Time to find: 2509.1645250320435
 
 148,934 primes found
+148,934th prime found is 1,999,993
 """
