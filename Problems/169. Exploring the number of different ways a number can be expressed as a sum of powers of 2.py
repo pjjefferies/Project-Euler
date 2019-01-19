5a7045798e23bad59c3bdd9c968f23f34152d703
Paul Jefferies <jefferie@umich.edu>
2019-01-18 23:35:50 -0700

    Update problem 169: NG solution as solves in O(x**N)

--- Problems/169. Exploring the number of different ways a number can be expressed as a sum of powers of 2.py
+++ Problems/169. Exploring the number of different ways a number can be expressed as a sum of powers of 2.py
@@ -4,7 +4,7 @@ Project Euler
 
 Exploring the number of different ways a number can be expressed as a sum of
 powers of 2
-Problem 169 
+Problem 169s
 
 Define f(0)=1 and f(n) to be the number of different ways n can be expressed
 as a sum of integer powers of 2 using each power no more than twice.
@@ -17,7 +17,7 @@ For example, f(10)=5 since there are five different ways to express 10:
 2 + 4 + 4
 2 + 8
 
-What is f(1025)?
+What is f(10**25)?
 
 Created on Fri Jan 18 13:00:00 2019
 
@@ -28,14 +28,72 @@ from time import time
 # import math
 
 
+def build_max_below(pow_2, max_value):
+    max_below = {1: 2}
+    # old_2_pow = 1
+    # while old_2_pow * 2 <= max_value:
+    for new_2_pow in pow_2[1:]:
+        old_2_pow = new_2_pow / 2
+        # new_2_pow = old_2_pow * 2
+        max_below[new_2_pow] = max_below[old_2_pow] + 2 * new_2_pow
+        old_2_pow = new_2_pow
+    return max_below
+
+
+def build_2_pow_list(max_sum):
+    pow_2 = [1]
+    while pow_2[-1] < max_sum:
+        pow_2.append(pow_2[-1] * 2)
+    pow_2 = pow_2[:-1]
+    return pow_2
+
+
+def build_sum_below_dict(n):
+    pass
+
+
 if __name__ == '__main__':
     startTime = time()
     print('\n')
+    max_sum = 10000  # 1e25
 
+    pow_2s = build_2_pow_list(max_sum)
 
+    # max_below = build_max_below(pow_2s, max_sum)
+
+    sums_below = {1: {0: [[0]],
+                      1: [[1]],
+                      2: [[2]]}}
+
+    for this_2_pow in range(1, len(pow_2s)):  # 1, 2, 3, 4, 5, ...
+        prev_2_pow = this_2_pow - 1        # 0, 1, 2, 3, 4, ...
+        # print('this_2_pow:', this_2_pow)
+        this_pow_2 = 2 ** this_2_pow  # e.g 2, 4, 8, 16, 32, ...
+        prev_pow_2 = int(this_pow_2 / 2)  # 1, 2, 4, 8, 16, 32, ...
+        these_sums = {}
+        # print('sums_below[prev_pow_2]:', sums_below[prev_pow_2])
+        for a_prev_sum_below in sums_below[prev_pow_2]:
+            # print('a_prev_sum_below:', a_prev_sum_below)
+            for a_prev_sum_combo in sums_below[prev_pow_2][a_prev_sum_below]:
+                for no_new_2_pow in range(2+1):
+                    new_sum = a_prev_sum_below + this_pow_2 * no_new_2_pow
+                    if new_sum in these_sums:
+                        curr_new_sum_combo = these_sums[new_sum]
+                        curr_new_sum_combo.append(
+                            a_prev_sum_combo + [no_new_2_pow])
+                    else:
+                        curr_new_sum_combo = [a_prev_sum_combo +
+                                              [no_new_2_pow]]
+                    # print('curr_new_sum_combo:', curr_new_sum_combo,
+                    #       ', new_sum:', new_sum)
+                    these_sums[new_sum] = curr_new_sum_combo
+        # print('this_pow_2:', this_pow_2, ', these_sums:\n', these_sums)
+        # sums_below[this_pow_2] = these_sums
+        sums_below = {this_pow_2: these_sums.copy()}
+        # print('sums_below:\n', sums_below)
 
     totalTime = time() - startTime
-    print('\n')
+    print('\nf(' + str(max_sum) + ') = ', len(sums_below[this_pow_2][max_sum]))
     print('Time to find:', '{:,.3f} s'.format(totalTime))
 
 """
